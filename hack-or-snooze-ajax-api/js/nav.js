16a3e09f9a7eb37f32c1b68c9f6b20cce5da1249
Juliane Cassidy <julianecassidy@gmail.com>
2023-06-29 19:20:05 -0700

    add hack-or-snooze

---
+++ hack-or-snooze-ajax-api/js/nav.js
@@ -0,0 +1,58 @@
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  evt.preventDefault();
+  hidePageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show the form to add a new story when click "submit" */
+
+function showNewStoryForm(evt) {
+  console.debug("showNewStoryForm", evt);
+  evt.preventDefault;
+  $storyForm.show();
+}
+
+$body.on("click", "#new-story", showNewStoryForm);
+$body.on("submit", "#story-form", function(evt) {
+  evt.preventDefault;
+  $storyForm.hide();
+  handleNewStoryFormSubmit();
+})
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  evt.preventDefault();
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+}
+
+$navLogin.on("click", navLoginClick);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $navUserProfile.text(`${currentUser.username}`).show();
+
+  $newStory.show();
+}
+
+// $allStoriesList.on("click", ".bi-star", handleAddFavoriteStory);
+// $allStoriesList.on("click", ".bi-star-fill", handleRemoveFavoriteStory);
---
+++ hack-or-snooze-ajax-api/js/stories.js
@@ -0,0 +1,97 @@
+"use strict";
+
+// This is the global list of the stories, an instance of StoryList
+let storyList;
+
+/** Get and show stories when site first loads. */
+
+async function getAndShowStoriesOnStart() {
+  storyList = await StoryList.getStories();
+  $storiesLoadingMsg.remove();
+
+  putStoriesOnPage();
+}
+
+/**
+ * A render method to render HTML for an individual Story instance
+ * - story: an instance of Story
+ *
+ * Returns the markup for the story.
+ */
+
+function generateStoryMarkup(story) {
+  // console.debug("generateStoryMarkup", story);
+
+  const hostName = story.getHostName(story.url);
+  const userFavoriteIds = currentUser.favorites.map(favorite => favorite.storyId);
+  const star = userFavoriteIds.includes(story.storyId) ? "bi-star-fill" : "bi-star";
+  // const star = "bi-star";
+
+  return $(`
+      <li id="${story.storyId}">
+        <a href="${story.url}" target="a_blank" class="story-link">
+          ${story.title}
+        </a>
+        <small class="story-hostname">(${hostName})</small>
+        <small class="story-author">by ${story.author}</small>
+        <small class="story-user">posted by ${story.username}</small>
+        <i class="bi ${star}"></i>
+      </li>
+    `);
+}
+
+/** Gets list of stories from server, generates their HTML, and puts on page. */
+
+function putStoriesOnPage() {
+  console.debug("putStoriesOnPage");
+
+  $allStoriesList.empty();
+
+  // loop through all of our stories and generate HTML for them
+  for (let story of storyList.stories) {
+    const $story = generateStoryMarkup(story);
+    $allStoriesList.append($story);
+  }
+
+  $allStoriesList.show();
+}
+
+function putStoryOnPage(story) {
+  console.debug("putStoryOnPage", story);
+
+  const $formattedStory = generateStoryMarkup(story);
+  $allStoriesList.prepend($formattedStory);
+}
+
+/** Handle submit of new story by calling addStory with the form data and then 
+ * calling putStoryOnPage
+ */
+
+async function handleNewStoryFormSubmit() {
+  console.debug("handleNewStoryFormSubmit");
+
+  const newStoryData = {
+    author: $("#author").val(),
+    title: $("#title").val(),
+    url: $("#url").val()
+  }
+
+  const newStory = await storyList.addStory(currentUser, newStoryData);
+
+  putStoryOnPage(newStory);
+}
+
+async function handleAddFavoriteStory() {
+  return console.log("coming soon");
+
+}
+
+
+async function handleRemoveFavoriteStory() {
+  return console.log("coming soon");
+}
+
+
+
+
+
